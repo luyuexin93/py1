ebc2766fd9520481f02aa7421f3efe7ca5788ef5
Lyx <luyuexin93@outlook.com>
2018-07-09 17:34:24 +0800

    interface test

---
+++ interface_test/Readme.md
@@ -0,0 +1,15 @@
+#python 接口自动化 尝试
+
+
+ 1. 完成HTMLtestrunner email 代码demo 调试
+ 2. 接口测试框架主体待完成。。
+ 3. 依赖包说明
+    - requests 模拟http请求 用于接口测试
+    - email smtplib 自带 用于收发邮件
+    - HTMLtestrunner 配合unittest 生成html报告 需下载
+    - unittest 自带 python单元测试库
+
+HTMLtestrunner 默认Python2 版本 适配python3
+[参考](https://www.cnblogs.com/my-blogs-for-everone/p/6058390.html) 
+
+[python接口自动化样例](https://blog.csdn.net/sxyzwq/article/details/62039952?locationNum=1&fps=1)
---
+++ testmail.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+from email.mime.text import MIMEText
+from email.header import Header
+import smtplib
+
+msg=MIMEText('hello ,send by Python...','plain','utf-8')
+subject='python email test'
+msg['Subject']=Header(subject,'utf-8')
+from_addr = 'luyuexin@zjipst.com'
+password = 'Lu123456'
+to_addr = 'luyuexin@zjipst.com'
+smtp_server='mail.zjipst.com'
+
+server = smtplib.SMTP()
+#server.set_debuglevel(1)
+server.connect(smtp_server)
+server.login(from_addr,password)
+server.sendmail(from_addr,[to_addr],msg.as_string())
+server.quit()
